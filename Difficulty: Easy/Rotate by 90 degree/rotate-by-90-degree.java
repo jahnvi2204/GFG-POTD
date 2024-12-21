df1b802c8ae08ec79dfb0f3418f4356408e11443
JAHNVI SAXENA <118651832+jahnvi2204@users.noreply.github.com>
2024-12-21 23:36:57 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Rotate by 90 degree/rotate-by-90-degree.java
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int matrix[][] = new int[n][n];
+            // String st[] = read.readLine().trim().split("\\s+");
+            int k = 0;
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) matrix[i][j] = sc.nextInt();
+            }
+            Solution ob = new Solution();
+            ob.rotateby90(matrix);
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) System.out.print(matrix[i][j] + " ");
+                System.out.println();
+            }
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    // Function to rotate matrix anticlockwise by 90 degrees.
+    static void rotateby90(int arr[][]) {
+        // code here
+          int len=arr.length;
+        // reverese the array
+        for(int i=0;i<len;i++){
+           int st=0,lst=len-1;
+           while(lst>st){
+               int temp=arr[i][st];
+               arr[i][st]=arr[i][lst];
+               arr[i][lst]=temp;
+               st++;
+               lst--;
+           }
+        }
+        // take transpose
+        for(int i=0;i<len;i++){
+            for(int j=0;j<=i;j++){
+                int temp=arr[i][j];
+                arr[i][j]=arr[j][i];
+                arr[j][i]=temp;
+            }
+        }
+    }
+}
