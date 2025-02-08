dafecc851d78b6e0608140bf1fb4a8d2d63cf402
JAHNVI SAXENA <118651832+jahnvi2204@users.noreply.github.com>
2025-02-08 15:57:45 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Tree Boundary Traversal/tree-boundary-traversal.java
@@ -0,0 +1,152 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Node {
+    int data;
+    Node left, right;
+
+    public Node(int d) {
+        data = d;
+        left = right = null;
+    }
+}
+
+class GFG {
+    static Node buildTree(String str) {
+        // Corner Case
+        if (str.length() == 0 || str.equals('N')) return null;
+        String[] s = str.split(" ");
+
+        Node root = new Node(Integer.parseInt(s[0]));
+        Queue<Node> q = new LinkedList<Node>();
+        q.add(root);
+
+        // Starting from the second element
+        int i = 1;
+        while (!q.isEmpty() && i < s.length) {
+            // Get and remove the front of the queue
+            Node currNode = q.remove();
+
+            // Get the current node's value from the string
+            String currVal = s[i];
+
+            // If the left child is not null
+            if (!currVal.equals("N")) {
+
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+
+                // Push it to the queue
+                q.add(currNode.left);
+            }
+
+            // For the right child
+            i++;
+            if (i >= s.length) break;
+            currVal = s[i];
+
+            // If the right child is not null
+            if (!currVal.equals("N")) {
+
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+
+                // Push it to the queue
+                q.add(currNode.right);
+            }
+
+            i++;
+        }
+
+        return root;
+    }
+
+    public static void main(String args[]) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t > 0) {
+            String s = br.readLine();
+            Node root = buildTree(s);
+
+            Solution T = new Solution();
+
+            ArrayList<Integer> res = T.boundaryTraversal(root);
+            for (Integer num : res) System.out.print(num + " ");
+            System.out.println();
+            t--;
+
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+/*
+class Node
+{
+    int data;
+    Node left, right;
+
+    public Node(int d)
+    {
+        data = d;
+        left = right = null;
+    }
+}
+*/
+
+class Solution {
+    ArrayList<Integer> boundaryTraversal(Node node) {
+        ArrayList<Integer> res = new ArrayList<>();
+        if (node == null) return res;
+
+        if (!isLeaf(node)) res.add(node.data);
+
+        addLeft(node, res);
+        addLeaf(node, res);
+        addRight(node, res);
+
+        return res;
+    }
+
+    void addLeft(Node root, ArrayList<Integer> res) {
+        Node curr = root.left;
+        while (curr != null) {
+            if (!isLeaf(curr)) res.add(curr.data);
+            if (curr.left != null) curr = curr.left;
+            else curr = curr.right;
+        }
+    }
+
+    void addRight(Node root, ArrayList<Integer> res) {
+        ArrayList<Integer> temp = new ArrayList<>();
+        Node curr = root.right;
+        while (curr != null) {
+            if (!isLeaf(curr)) temp.add(curr.data);
+            if (curr.right != null) curr = curr.right;
+            else curr = curr.left;
+        }
+        for (int i = temp.size() - 1; i >= 0; i--) {
+            res.add(temp.get(i));
+        }
+    }
+
+    boolean isLeaf(Node curr) {
+        return curr.left == null && curr.right == null;
+    }
+
+    void addLeaf(Node curr, ArrayList<Integer> res) {
+        if (isLeaf(curr)) {
+            res.add(curr.data);
+            return;
+        }
+        if (curr.left != null) addLeaf(curr.left, res);
+        if (curr.right != null) addLeaf(curr.right, res);
+    }
+}
+
