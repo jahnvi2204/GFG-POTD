89518212d0441a661f45cf01892f7ea1d2ba40ff
JAHNVI SAXENA <118651832+jahnvi2204@users.noreply.github.com>
2025-03-15 21:57:16 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Indexes of Subarray Sum/indexes-of-subarray-sum.java
+++ Difficulty: Medium/Indexes of Subarray Sum/indexes-of-subarray-sum.java
@@ -33,33 +33,40 @@ class Main {
 
 
 
-class Solution {
-    static ArrayList<Integer> subarraySum(int[] arr, int s) {
-        int n=arr.length;
-        int l = 0;
-        int r = 0;
-        int sum = 0;
 
+class Solution {
+    // Function to find a continuous sub-array which adds up to
+    // a given number.
+    static ArrayList<Integer> subarraySum(int[] arr, int target) {
         ArrayList<Integer> res = new ArrayList<>();
-        while(r < n){
-            sum += arr[r];
-            while(sum > s && l<n){ 
-                sum -= arr[l];
-                l++;
+        int n = arr.length;
+
+        // Pick a starting point for a subarray
+        for (int s = 0; s < n; s++) {
+            int curr = 0;
+
+            // Consider all ending points
+            // for the picked starting point
+            for (int e = s; e < n; e++) {
+                curr += arr[e];
+                if (curr == target) {
+                    res.add(s + 1);
+                    res.add(e + 1);
+                    return res;
+                }
             }
-            if(sum == s && r>=l ){
-                res.add(l + 1);
-                res.add(r + 1); 
-               break;
-            }
-            r++;
         }
-        if(res.size() == 0 || l> r){
-            res.clear();
-            res.add(-1);
-    }  
-    return res;
+        // If no subarray is found
+        res.add(-1);
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {15, 2, 4, 8, 9, 5, 10, 23};
+        int target = 23;
+        ArrayList<Integer> res = subarraySum(arr, target);
+
+        for (int ele : res)
+            System.out.print(ele + " ");
+    }
 }
-}
-        // code here
-    
